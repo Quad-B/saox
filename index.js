9a250d86182a2162d2b00097460c74c1244da4cf
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2017-07-03 00:17:31 +0700

    add change and delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const {session} = require('electron')
 
 // init win
 let win;
@@ -10,20 +11,15 @@ function createWindow() {
 	win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/saoxlogo.png'})
 
 	// Load index.html
-	win.loadURL('http://music.sanook.com/music/');
-
-	win.webContents.on('new-window', function(e, url) {
-  	e.preventDefault();
-  	require('electron').shell.openExternal(url);
-	});
+	win.loadURL('http://music.sanook.com');
 
 	win.webContents.on('did-finish-load', function() {
- 	win.webContents.insertCSS('.sn-header{ display: none;}')
+ 	win.webContents.insertCSS('.sn-header{ display: none;} .dialog-col--register {display: none;} .span-6 {width: 100% !important;}')
 	});
 
-	win.on('closed', () => {
-		win = null;
-	});
+	session.defaultSession.cookies.get({}, (error, cookies) => {
+  	console.log(error, cookies)
+	})
 }
 
 // Run create window function
