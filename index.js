09537c9c7d95b358bb42bb1aa338c3c15e5c1f19
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2017-07-09 00:37:53 +0700

    add ia32 new version and add kill process

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
-	if(process.platform !== 'win32'){
+	if(process.platform !== 'darwin'){
 		app.quit();
 	}
 });
