c2d233272c58e1c1c6786dea4cdee374974f643a
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-01-28 02:57:50 +0700

    1.8.4

--- index.js
+++ index.js
@@ -95,10 +95,6 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-
 autoUpdater.on('update-available', () => {
   win.webContents.send('update_available');
 });
