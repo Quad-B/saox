607b359ef6f8c7f2aa11aa9513a4522b87d4b876
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-01-27 12:11:35 +0700

    1.8.0

--- index.js
+++ index.js
@@ -48,7 +48,12 @@ app.commandLine.appendSwitch('disable-site-isolation-trials')
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+//app.on('ready', createWindow);
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+  createWindow();
+});
 
 // Quit when all windows are closed.
 /*app.on('window-all-closed', () => {
@@ -69,7 +74,6 @@ app.on('before-quit', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  autoUpdater.checkForUpdatesAndNotify();
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
