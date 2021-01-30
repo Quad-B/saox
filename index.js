b92136ee3153cf0303166e6b3e1d31771d51bc27
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-01-30 11:02:33 +0700

    2.1.5

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function createWindow () {
   });*/
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+   win.webContents.openDevTools()
 
   // session.defaultSession.cookies.get({}, (error, cookies) => {
   //  console.log(error, cookies)
