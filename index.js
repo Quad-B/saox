a57690bc78c0cdf03b1303ce45d6cd0b4a69fd90
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-07-15 01:29:01 +0700

    2.7.37

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function createWindow() {
     icon: __dirname + '/img/saoxlogo.png',
     width: 800,
     height: 600,
-    autoHideMenuBar: true,
-    frame: false,
+    autoHideMenuBar: false,
+    //autoHideMenuBar: true,
+    //frame: false,
+    frame: true,
     webPreferences: {
       nodeIntegration: true,
       nativeWindowOpen: true,
